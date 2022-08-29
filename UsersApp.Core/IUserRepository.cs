a57e9c7e83b9896cf5655a35f0bc2ae5a136b920
Maroš Kahan <maros.kahan@m2ms.sk>
2022-08-29 11:27:07 +0200

    Core project implemenatation - added model with DB first approach, added UserRepository interface

---
+++ UsersApp.Core/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp.Core
+{
+    public interface IUserRepository
+    {
+        void Create(User user);
+        void Update(User user);
+        void Delete(int userId);
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int userId);
+    }
+}
